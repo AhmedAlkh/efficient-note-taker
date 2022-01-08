a4cb2ef72e7d93949ad4ed6ce4c8dca2e64bf1f9
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-01-07 21:18:20 -0500

    added get routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const notesFile = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,6 +12,27 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
+
+app.get('/api/notes', (req, res) => {
+    res.json(notesFile.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// links to file apiRoutes in routes folder
+// app.use('/api', apiRoutes);
+// links to file htmlRoutes in routes folder
+// app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
